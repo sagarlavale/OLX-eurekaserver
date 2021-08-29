4a0b193f6e9833d184d2dec6ca5a5f7c912808cd
SL62206 <sagar.lavale@zensar.com>
2021-08-29 22:22:15 +0530

    Initial commit

---
+++ src/main/java/com/zensar/eureka/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zensar.eureka;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OlxEurekaserverApplication.class);
+	}
+
+}
